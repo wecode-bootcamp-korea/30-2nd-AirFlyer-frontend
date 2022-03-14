5974a8a1fd3e0e7753617c84bb0a647c3bd0985d
hyejin30 <tyui3185@gmail.com>
2022-03-15 07:32:27 +0900

    Nav 레이아웃/스타일링 완료, 공통 부분 삭제, Icon 컴포넌트 재사용
    Fix : Icon 컴포넌트 재사용

---
+++ src/components/Nav/navdata.js
@@ -0,0 +1,41 @@
+export const NAV_LIST = {
+  menuLinks: [
+    {
+      id: 1,
+      name: '예매',
+      to: '/',
+    },
+    {
+      id: 2,
+      name: '공항',
+      to: '/airport',
+    },
+    {
+      id: 3,
+      name: '기내',
+      to: '/inflight',
+    },
+    {
+      id: 4,
+      name: '스카이패스',
+      to: '/skypass',
+    },
+  ],
+  iconLinks: [
+    {
+      id: 1,
+      name: 'alram',
+      src: '/images/alram.png',
+    },
+    {
+      id: 2,
+      name: 'cart',
+      src: '/images/cart.png',
+    },
+    {
+      id: 3,
+      name: 'search',
+      src: '/images/search.png',
+    },
+  ],
+};
